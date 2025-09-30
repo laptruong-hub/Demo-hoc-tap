4a18dddb410729aa1d3a23d255bc67fe3cb5f7ee
laptruong-hub <laptqse183589@fpt.edu.vn>
2025-09-30 11:23:58 +0700

    Them bai tap 3

---
+++ src/baptap3/baitap3.js
@@ -0,0 +1,13 @@
+console.log("baitap 3");
+
+//Object
+const person = {
+    name: "Lập Trương",
+    age: 20,
+    address: "TPHCM"
+}
+console.log(person);
+
+//Array
+const scores = [9, 8, 7, 6, 5];
+console.log(scores);

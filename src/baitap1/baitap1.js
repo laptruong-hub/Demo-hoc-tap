0ed7a7d8deb607ce8fb7a641c2c36477693cbb62
laptruong-hub <laptqse183589@fpt.edu.vn>
2025-09-30 11:01:12 +0700

    Them bai tap

---
+++ src/baitap1/baitap1.js
@@ -0,0 +1,4 @@
+console.log("baitap 12");
+var name = 'laptruong';
+var name2 = "laptruong1";
+console.log(name);
---
+++ src/baitap2.html/baitap2.js
@@ -0,0 +1,24 @@
+console.log("baitap 2")
+
+
+// Khai báo biến
+const myAge = 20;
+const myScore = 9.5;
+console.log(myAge);
+console.log(myScore);
+
+//String
+const name = "Lập Trương";
+console.log(name);
+
+// Boolean : true/false
+const isLapTruong = true;
+console.log(isLapTruong);
+
+//undefined cho biết biến chưa được gán giá trị
+let bla;
+console.log(bla);
+
+//null cho biết biến có giá trị là null
+let testNull = null;
+console.log(testNull);
